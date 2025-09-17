60cee7b10b68b37be65815080f4bf15c3542e657
Aditya Thakkar <adityaat@iisc.ac.in>
2025-09-17 09:31:41 +0530

    Resolved all TODOs

--- src/bin/chapter_4.rs
+++ src/bin/chapter_4.rs
@@ -27,24 +27,48 @@ fn main() {
     println!("{}, world!", s2); // WORKS
     // println!("{}, world!", s1); // DOES NOT WORK
     // REASON: s1 has been moved into s2, and hence s1 is dropped.
-    // TODO 1: add the string addition example here. It removes ownership from the lhs variable of the + operation. 
+
+    // String addition removes ownership from the lhs variable of the + operation.
     // (RHS of + operation is a reference, which anyway does not have ownership.)
-    // TODO 2: in the example above, use MyString (or MyBox), with a print statement inside the drop() method,
+    let l_str = String::from("Hello");
+    let r_str = String::from(", world!");
+    let concat_str = l_str + &r_str;
+    println!("{}", concat_str);
+    println!("{}", r_str);
+    // println!("{}", l_str); // ERROR!!!
+
+    // DONE: in the example above, use MyString (or MyBox), with a print statement inside the drop() method,
     // and check that at runtime the printing happens only once. This verifies that the compiler
     // does not call the drop() method on places that no longer own their value.
+    #[derive(Debug)]
+    struct MyBox<T>(T);
+    impl<T> Drop for MyBox<T> {
+        fn drop(&mut self) {
+            println!("Dropping MyBox");
+        }
+    }
+    let box1 = MyBox(5);
+    let box2 = box1;
+    println!("{:?}", box2); // MyBox(5)
+    // NOTE: Dropping MyBox is printed exactly once at the end of the output.
 
-    // Let me confuse you
     let num1 = 1;
     let num2 = num1;
     println!("{}", num2); // WORKS
     println!("{}", num1); // WORKS!!
     // REASON: i32 implements the Copy trait, hence instead of moving num1 into num2, it copies the value of num1 into num2
 
-    // TODO 3: Can you show a struct with COPY trait? Note: a struct that does not implement COPY trait defaults to MOVE semantics.
-
-    // Now comes the most interesting part of the chapter
-    // References and Borrowing
-    // Let's discuss it in a separate file
+    // A struct with COPY trait.
+    // Note: a struct that does not implement COPY trait defaults to MOVE semantics.
+    // NOTE: Every type which implements the Copy trait must also implement the Clone trait.
+    #[derive(Debug, Copy, Clone)]
+    struct CopyableStruct {
+        id: u8,
+    }
+    let copy1 = CopyableStruct { id: 1 };
+    let copy2 = copy1;
+    println!("{:?}", copy1);
+    println!("{:?}", copy2);
 
     // THIS COMPILES, due to copy semantics
     let x = 1;
@@ -52,18 +76,19 @@ fn main() {
     println!("{}", x);
 
     // THIS DOES NOT, due to MOVE semantics
-    // let a = String::from([1,2,3]);
-    // let b = a;
-    // println!("{}", a);
+    let a = String::from([1,2,3]);
+    let b = a;
+    // println!("{}", a); // ERROR!!!
 
 
-    // TODO Move this to 4_2 or 10_3
-    let mut first = String::from("first");
-    do_nothing(&mut first);
-    println!("{}", first);
+    // The function `do_nothing` takes the ownership of first.
+    let first = String::from("first");
+    do_nothing(first);
+    // println!("{}", first); // ERROR!!!
 }
 
-fn do_nothing(first_parameter: &mut String) {
+fn do_nothing(mut first_parameter: String) {
     println!("Noting {}", first_parameter);
     first_parameter.push_str(", second");
+    println!("{}", first_parameter);
 }
